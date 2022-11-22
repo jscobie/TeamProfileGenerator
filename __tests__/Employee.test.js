e87482571d382173e07a949d110d2f0bd233200e
Jacob Scobie <jscobie@focus-solutions.net>
2022-11-21 22:10:54 -0500

    updated testing to add describes

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,48 +1,50 @@
 const Employee = require('../lib/Employee');
 
-test('Can we create a new Employee object?', () => {
-    const employeeTest = new Employee();
-    expect(typeof(employeeTest)).toBe('object');
-})
-
-test('Can we pass in a name and access it?', () => {
-    const name = 'Jacob';
-    const employeeTest = new Employee('Jacob', '3', 'test@contoso.com');
-    expect(employeeTest.name).toBe(name);
-})
-
-test('Can we pass in an ID and access it?', () => {
-    const id = '3';
-    const employeeTest = new Employee('Jacob', '3', 'test@contoso.com');
-    expect(employeeTest.id).toBe(id);
-})
-
-test('Can we pass in a name and access it?', () => {
-    const email = 'test@contoso.com';
-    const employeeTest = new Employee('Jacob', '3', 'test@contoso.com');
-    expect(employeeTest.email).toBe(email);
-})
-
-test('Test the getName function is able to return the name', () => {
-    const name = 'Jacob';
-    const employeeTest = new Employee('Jacob', '3', 'test@contoso.com');
-    expect(employeeTest.getName()).toBe(name);
-})
-
-test('Test the getID function is able to return the ID', () => {
-    const id = '3';
-    const employeeTest = new Employee('Jacob', '3', 'test@contoso.com');
-    expect(employeeTest.getId()).toBe(id);
-})
-
-test('Test the getEmail function is able to return the email', () => {
-    const email = 'test@contoso.com';
-    const employeeTest = new Employee('Jacob', '3', 'test@contoso.com');
-    expect(employeeTest.getEmail()).toBe(email);
-})
-
-test('Can we use the getRole function to return the employee role', () => {
-    const role = 'Employee';
-    const employeeTest = new Employee('Jacob', '3', 'test@contoso.com');
-    expect(employeeTest.getRole()).toBe(role);
+describe('Employee',()  => {
+    test('Can we create a new Employee object?', () => {
+        const employeeTest = new Employee();
+        expect(typeof(employeeTest)).toBe('object');
+    })
+    
+    test('Can we pass in a name and access it?', () => {
+        const name = 'Jacob';
+        const employeeTest = new Employee('Jacob', '3', 'test@contoso.com');
+        expect(employeeTest.name).toBe(name);
+    })
+    
+    test('Can we pass in an ID and access it?', () => {
+        const id = '3';
+        const employeeTest = new Employee('Jacob', '3', 'test@contoso.com');
+        expect(employeeTest.id).toBe(id);
+    })
+    
+    test('Can we pass in a name and access it?', () => {
+        const email = 'test@contoso.com';
+        const employeeTest = new Employee('Jacob', '3', 'test@contoso.com');
+        expect(employeeTest.email).toBe(email);
+    })
+    
+    test('Test the getName function is able to return the name', () => {
+        const name = 'Jacob';
+        const employeeTest = new Employee('Jacob', '3', 'test@contoso.com');
+        expect(employeeTest.getName()).toBe(name);
+    })
+    
+    test('Test the getID function is able to return the ID', () => {
+        const id = '3';
+        const employeeTest = new Employee('Jacob', '3', 'test@contoso.com');
+        expect(employeeTest.getId()).toBe(id);
+    })
+    
+    test('Test the getEmail function is able to return the email', () => {
+        const email = 'test@contoso.com';
+        const employeeTest = new Employee('Jacob', '3', 'test@contoso.com');
+        expect(employeeTest.getEmail()).toBe(email);
+    })
+    
+    test('Can we use the getRole function to return the employee role', () => {
+        const role = 'Employee';
+        const employeeTest = new Employee('Jacob', '3', 'test@contoso.com');
+        expect(employeeTest.getRole()).toBe(role);
+    })
 })
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,19 +1,21 @@
 const Intern = require('../lib/Intern');
 
-test('Can we pass in a githuburl and access it?', () => {
-    const testSchool = 'College';
-    const employeeInstance = new Intern('Jacob', 3, 'test@contoso.com', testSchool);
-    expect(employeeInstance.school).toBe(testSchool);
-});
+describe('Intern',()  => {
+    test('Can we pass in a githuburl and access it?', () => {
+        const testSchool = 'College';
+        const employeeInstance = new Intern('Jacob', 3, 'test@contoso.com', testSchool);
+        expect(employeeInstance.school).toBe(testSchool);
+    });
 
-test('Test the getSchool function is able to return the School', () => {
-    const testSchool = 'College';
-    const employeeInstance = new Intern('Jacob', 3, 'test@contoso.com', testSchool);
-    expect(employeeInstance.getSchool()).toBe(testSchool);
-});
+    test('Test the getSchool function is able to return the School', () => {
+        const testSchool = 'College';
+        const employeeInstance = new Intern('Jacob', 3, 'test@contoso.com', testSchool);
+        expect(employeeInstance.getSchool()).toBe(testSchool);
+    });
 
-test('Test the getRole function is able to return the role', () => {
-    const returnValue = 'Intern';
-    const employeeInstance = new Intern('Jacob', 3, 'test@contoso.com', 'College');
-    expect(employeeInstance.getRole()).toBe(returnValue);
+    test('Test the getRole function is able to return the role', () => {
+        const returnValue = 'Intern';
+        const employeeInstance = new Intern('Jacob', 3, 'test@contoso.com', 'College');
+        expect(employeeInstance.getRole()).toBe(returnValue);
+    });
 });
